3a67aaffcbf63d4c8fed7eb8aa0adbe624791699
Raymond.L <93538228+liaoh1@users.noreply.github.com>
2023-11-10 13:53:53 +0000

    Delete build directory
    you don't have to have it

--- build/CMakeFiles/us.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for us.
-# This may be replaced when dependencies are built.
--- build/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/haichenliao/Desktop/unitTest/group3/googletest/googlemock
-# Build directory: /Users/haichenliao/Desktop/unitTest/group3/build/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
