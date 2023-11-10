322d7c85dd5e003673fb79c5ba564276a05d69f0
Raymond.L <93538228+liaoh1@users.noreply.github.com>
2023-11-10 17:02:32 +0000

    Update gs.cpp

--- gs.cpp
+++ gs.cpp
@@ -149,27 +149,6 @@ TEST(SimulationTest, HandlesZeroInput) {
     }
 }
 
-TEST(SimulationTest, HandlesZeroInput) {
-    const int width = 256;
-    const int height = 256;
-    const double dt = 0.06;
-    std::vector<std::vector<double>> u, v;
-
-    u = std::vector<std::vector<double>>(width, std::vector<double>(height, 0.0));
-    v = std::vector<std::vector<double>>(width, std::vector<double>(height, 0.0));
-
-    // Run the simulateStep
-    simulateStep();
-
-    // Check that u and v are still zero after simulateStep function called
-    for (int i = 0; i < width; ++i) {
-        for (int j = 0; j < height; ++j) {
-            EXPECT_DOUBLE_EQ(0.0, u[i][j]) << "u[" << i << "][" << j << "] is not zero.";
-            EXPECT_DOUBLE_EQ(0.0, v[i][j]) << "v[" << i << "][" << j << "] is not zero.";
-        }
-    }
-}
-
 int main(int argc, char* argv[]) {
     ::testing::InitGoogleTest(&argc, argv);
     // if (argc != 5){
